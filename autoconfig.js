71f16c066a85a3db6d26bb55a8e889a78b5a0ef7
Nikolaos Vardalakis <45063685+NikVard@users.noreply.github.com>
2021-08-27 11:29:01 +0200

    Initial upload
    Autoconfig.js and myfox.cfg

---
+++ autoconfig.js
@@ -0,0 +1,7 @@
+// Sets up the .cfg file
+
+//Specifies the name of the AutoConfig file
+pref("general.config.filename", "myfox.cfg");
+
+//Allows .cfg file to be stored in plain text without obfuscation
+pref("general.config.obscure_value", 0);
